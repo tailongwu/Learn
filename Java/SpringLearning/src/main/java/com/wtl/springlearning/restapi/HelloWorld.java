fdaf75e4cc0ce3009eb32dc8c0b73d94047f714f
Tailong Wu <wu_tailong@hotmail.com>
2020-01-08 23:39:59 +0800

    Add Java Spring hello world

---
+++ Java/SpringLearning/src/main/java/com/wtl/springlearning/restapi/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.wtl.springlearning.restapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world";
+    }
+}
