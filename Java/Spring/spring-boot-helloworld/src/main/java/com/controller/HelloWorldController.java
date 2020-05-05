e0601557f7e9e4dadf0d077288242949bf33cf6e
Tailong Wu <wu_tailong@hotmail.com>
2020-05-06 00:05:37 +0800

    feat: mess

---
+++ Java/Spring/spring-boot-helloworld/src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String helloworld() {
+        return "hello world";
+    }
+}
