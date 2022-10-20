622b44c7a381b9fbea1895b7f183d389ceb00ece
Tailong Wu <wu_tailong@hotmail.com>
2022-10-20 23:09:27 +0800

    [DesignPattern] add more virtual wallet functions

--- Geek/DesignPattern/ch12/virtual_wallet_entity.go
+++ Geek/DesignPattern/ch12/virtual_wallet_entity.go
@@ -3,13 +3,24 @@ package ch12
 import "errors"
 
 type VirtualWallet struct {
-	Id         int64
-	CreateTime int64
-	Balance    float64
+	Id                 int64
+	CreateTime         int64
+	Balance            float64
+	IsAllowedOverdraft bool
+	OverdraftAmount    float64
+	FrozenAmount       float64
+}
+
+func (v *VirtualWallet) GetAvailableBalance() float64 {
+	total := v.Balance - v.FrozenAmount
+	if v.IsAllowedOverdraft {
+		total += v.OverdraftAmount
+	}
+	return total
 }
 
 func (v *VirtualWallet) Debit(amount float64) error {
-	if v.Balance < amount {
+	if v.Balance < v.GetAvailableBalance() {
 		return errors.New("...")
 	}
 	v.Balance -= amount
